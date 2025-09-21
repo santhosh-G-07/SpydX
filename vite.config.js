934d14821227a6cf35f78953ea1410dc023eb61d
thoppilshakthi <thoppilshakthi@gmail.com>
2025-09-21 23:36:25 +0530

    Initial commit for new site

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: '/', // 👈 CORRECT for custom domain
-  plugins: [react()],
-})
-
